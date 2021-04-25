826832ffb4532760ca20569670c2035e126b80b6
ponxpon <ponponpon816@gmail.com>
2021-04-25 14:29:10 +0000

    [Add]bookのindex,showページ、create,destroy機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,22 @@
 class BooksController < ApplicationController
+
   def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(id: current_user)
   end
 
   def show
+    @user = current_user
+    @book = Book.new
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -15,5 +26,13 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,19 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end
